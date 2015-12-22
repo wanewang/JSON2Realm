fdffc59024172afaf65967a08dc8617faca0d087
Wane Wang <jhihguan@gmail.com>
2015-12-22 23:34:06 +0800

    Initial Commit

---
+++ JSON2RealmTests/JSON2RealmTests.swift
@@ -0,0 +1,36 @@
+//
+//  JSON2RealmTests.swift
+//  JSON2RealmTests
+//
+//  Created by Wane Wang on 2015/12/22.
+//  Copyright © 2015年 Wane Wang. All rights reserved.
+//
+
+import XCTest
+@testable import JSON2Realm
+
+class JSON2RealmTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
